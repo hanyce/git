b6d8ed099c65fdc974a5839a9487f58928adf97d
zhangyihan <zhangyihan@kezaihui.com>
2019-04-18 18:10:17 +0800

    add new_test

--- git.py
+++ git.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019-04-03 17:32
 # @Author  : zhangyihan
-# @File    : git.py
 
 
-print('git123')
-print('git4566')
-print('git789')
-print('git123')
-print('zhangyihan')
-print('xuehan')
-print('git')
-
-
-def test():
-    data = []
-    test = []
-    return '101112'
+def new_test():
+    return 'zhangyihan'
